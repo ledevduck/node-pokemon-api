3359d6c9be2bf5b4c09604eb78b804e676738e26
“ledevduck” <devduck@tutanota.com>
2022-07-17 20:33:53 +0200

    refactor after exercice correction, 1h42m

--- app.js
+++ app.js
@@ -7,16 +7,16 @@ const port = 3000
 
 app.get('/', (req,res) => res.send('Hello again'))
 
+app.get('/api/pokemons', (req,res) => {
+    const message = 'Liste de pokémons récuperée'
+    res.json(success(message, pokemons))
+}) 
+
 app.get('/api/pokemons/:id', (req,res) => {
     const id = parseInt(req.params.id)
     const pokemon = pokemons.find(pokemon => pokemon.id === id)
     const message = 'Un pokémon a bien été trouvé'
     res.json(success(message, pokemon))
-})
-
-app.get('/api/pokemons', (req,res) => {
-    const message = 'Liste de pokémons récuperée'
-    res.json(success(message, pokemons))
 })
 
 app.listen(port, () => console.log(`App started on: http://localhost:${port}`))
