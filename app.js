3cc182d4f35ecd786ad3ce4fdbe2cbd9010d2762
“ledevduck” <devduck@tutanota.com>
2022-07-17 18:32:21 +0200

    first example of a basic server with express

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('hello')
+// get express
+const express = require('express')
+
+// express application instance, web server hosting rest API
+const app = express()
+// rest API will start on this port
+const port = 3000
+
+// first endpoint
+app.get('/', (req,res) => res.send('Hello'))
+
+// start rest API on port, and display message in console with listen method from express
+app.listen(port, () => console.log(`App started on: http://localhost:${port}`))
