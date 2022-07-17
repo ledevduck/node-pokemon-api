9bef82564215be1540c9c6e2246428a5361cc22b
“ledevduck” <devduck@tutanota.com>
2022-07-17 18:49:17 +0200

    script npm run start to execute nodemon

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req,res) => res.send('Hello'))
+app.get('/', (req,res) => res.send('Hello again'))
 
 app.listen(port, () => console.log(`App started on: http://localhost:${port}`))
