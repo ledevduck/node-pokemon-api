39290a19a477e741a9faacc560a0879764fe4200
“ledevduck” <devduck@tutanota.com>
2022-07-17 19:00:01 +0200

    add new route for example: ExpressEndpoint = app.METHOD(PATH, CALLBACK(req,res))

--- app.js
+++ app.js
@@ -5,4 +5,6 @@ const port = 3000
 
 app.get('/', (req,res) => res.send('Hello again'))
 
+app.get('/api/pokemons/1', (req,res) => res.send('Hello Pokemon1'))
+
 app.listen(port, () => console.log(`App started on: http://localhost:${port}`))
