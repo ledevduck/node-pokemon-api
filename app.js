50a5f44b821be51c7d843dec7ee3f491c77aff35
“ledevduck” <devduck@tutanota.com>
2022-07-17 19:35:15 +0200

    endpoint exercice to display total number of pokemons, 1h21m

--- app.js
+++ app.js
@@ -7,11 +7,15 @@ const port = 3000
 app.get('/', (req,res) => res.send('Hello again'))
 
 app.get('/api/pokemons/:id', (req,res) => {
-
-    // request returns string, cast it as a number with parseInt method for strict comparison with pokemon id
-    const id = parseInt(req.params.id, 10)
+    const id = parseInt(req.params.id)
     const pokemon = pokemons.find(pokemon => pokemon.id === id)
     res.send(`You asked for the pokemon ${pokemon.name}`)
+})
+
+// Exercice 1h21m
+// Endpoint displaying total number of pokemons
+app.get('/api/pokemons', (req,res) => {
+    res.send(`Il y a ${pokemons.length} pokémons`)
 })
 
 app.listen(port, () => console.log(`App started on: http://localhost:${port}`))
