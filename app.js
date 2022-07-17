020cdfbef8a7c87f155f1396a328750d6517095f
“ledevduck” <devduck@tutanota.com>
2022-07-17 19:21:00 +0200

    add mock pokemon ressource file, app js displays pokemon name from it with find method using id parameter from url

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const express = require('express')
+let pokemons = require('./mock-pokemon')
 
 const app = express()
 const port = 3000
 
 app.get('/', (req,res) => res.send('Hello again'))
 
-// :id in route path indicates it comes from client request
-// get id from url (client request) and displays it in response
 app.get('/api/pokemons/:id', (req,res) => {
     const id = req.params.id
-    res.send(`You asked for the pokemon ${id}`)
+    const pokemon = pokemons.find(pokemon => pokemon.id == id)
+    res.send(`You asked for the pokemon ${pokemon.name}`)
 })
 
 app.listen(port, () => console.log(`App started on: http://localhost:${port}`))
