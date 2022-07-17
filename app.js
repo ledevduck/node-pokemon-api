e615a9d4c0d75f7fecde5253f5c468bece664902
“ledevduck” <devduck@tutanota.com>
2022-07-17 20:22:44 +0200

    update app.js to use helper to send back message with json response

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const helper = require('./helper')
 let pokemons = require('./mock-pokemon')
 
 const app = express()
@@ -9,7 +10,8 @@ app.get('/', (req,res) => res.send('Hello again'))
 app.get('/api/pokemons/:id', (req,res) => {
     const id = parseInt(req.params.id)
     const pokemon = pokemons.find(pokemon => pokemon.id === id)
-    res.json(pokemon)
+    const message = 'Un pokémon a bien été trouvé'
+    res.json(helper.success(message, pokemon))
 })
 
 app.get('/api/pokemons', (req,res) => {
