5c0de1f9321f4a1394e91bda031098108e66a3c5
“ledevduck” <devduck@tutanota.com>
2022-07-17 18:46:40 +0200

    npm install nodemon --save-dev, nodemon to automate server reloading every file save (like live-server), --save-dev to save nodemon dependency for development only in package json, no need for it in production

--- app.js
+++ app.js
@@ -3,13 +3,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-// first endpoint
-// method get(firstArg, secondArg)
-// firstArg: rest API default route (empty url)
-// secondArg: function(req,res), returns response to client when calling endpoint
-// req: entry endpoint request object
-// res: response object returning from express to client
-// send method from response object to return 'Hello' message to client
 app.get('/', (req,res) => res.send('Hello'))
 
 app.listen(port, () => console.log(`App started on: http://localhost:${port}`))
