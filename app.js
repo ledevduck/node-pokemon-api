d42d35cd63ea1bb771ae5446fc431e7ea665ff81
“ledevduck” <devduck@tutanota.com>
2022-07-17 19:28:17 +0200

    cast request response as number for strict comparison with pokemon id because request returns string types only

--- app.js
+++ app.js
@@ -7,8 +7,10 @@ const port = 3000
 app.get('/', (req,res) => res.send('Hello again'))
 
 app.get('/api/pokemons/:id', (req,res) => {
-    const id = req.params.id
-    const pokemon = pokemons.find(pokemon => pokemon.id == id)
+
+    // request returns string, cast it as a number with parseInt method for strict comparison with pokemon id
+    const id = parseInt(req.params.id, 10)
+    const pokemon = pokemons.find(pokemon => pokemon.id === id)
     res.send(`You asked for the pokemon ${pokemon.name}`)
 })
 
