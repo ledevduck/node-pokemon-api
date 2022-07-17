57d57b9a8d4af9960c5d0ae7e2698922cd178a9f
“ledevduck” <devduck@tutanota.com>
2022-07-17 19:11:29 +0200

    modify express route to get param from url and displays it in response

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const port = 3000
 
 app.get('/', (req,res) => res.send('Hello again'))
 
-app.get('/api/pokemons/1', (req,res) => res.send('Hello Pokemon1'))
+// :id in route path indicates it comes from client request
+// get id from url (client request) and displays it in response
+app.get('/api/pokemons/:id', (req,res) => {
+    const id = req.params.id
+    res.send(`You asked for the pokemon ${id}`)
+})
 
 app.listen(port, () => console.log(`App started on: http://localhost:${port}`))
