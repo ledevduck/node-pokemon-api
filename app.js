480dfa0df318e1a9eee5d229fbebbfd5afb2ebff
ledevduck <devduck@tutanota.com>
2022-07-23 12:28:17 +0200

    add delete method

--- app.js
+++ app.js
@@ -46,4 +46,12 @@ app.put('/api/pokemons/:id', (req, res) => {
   res.json(success(message, pokemonUpdated))
 })
 
+app.delete('/api/pokemons/:id', (req, res) => {
+  const id = parseInt(req.params.id)
+  const pokemonDeleted = pokemons.find(pokemon => pokemon.id === id)
+  pokemons.filter(pokemon => pokemon.id != id)
+  const message = `Le pokemon ${pokemonDeleted.name} a bien été supprimé`
+  res.json(success(message, pokemonDeleted))
+})
+
 app.listen(port, () => console.log(`App started on: http://localhost:${port}`))
