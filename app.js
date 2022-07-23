1cf7c787cf01e7f56021d853a105195d1f51cbc7
ledevduck <devduck@tutanota.com>
2022-07-23 11:00:40 +0200

    import and use favicon middleware, before morgan

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require("express")
 const morgan = require('morgan')
+const favicon = require('serve-favicon')
 const { success } = require("./helper")
 let pokemons = require("./mock-pokemon")
 
 const app = express()
 const port = 3000
 
-app.use(morgan('dev'))
+app
+.use(favicon(__dirname + '/favicon.ico'))
+.use(morgan('dev'))
 
 app.get("/", (req, res) => res.send("Hello"))
 
